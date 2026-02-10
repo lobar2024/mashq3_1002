1b3ff87fb3bf500e24e632d65408415d273a2653
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-02-10 23:50:11 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+class Rectangle:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+
+    def area(self):
+        return self.w * self.h
+
+r = Rectangle(3, 4)
+print(r.area())  # 12class Rectangle:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+
+    def area(self):
+        return self.w * self.h
+
+r = Rectangle(3, 4)
+print(r.area())  # 12
